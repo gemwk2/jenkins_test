29da710a2b3d8a909da730c7e541506a544f68c7
gemwk2 <74185317+gemwk2@users.noreply.github.com>
2024-03-27 22:16:26 +0100

    Update netmiko_send_command.py

--- netmiko_send_command.py
+++ netmiko_send_command.py
@@ -1,1 +1,14 @@
+from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_command
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+def show_command_test(task):
+    result = task.run(task=netmiko_send_command, command_string="show ip interface brief")
+    # Možete obraditi rezultat ovde ako je potrebno
+    return result
+
+results = nr.run(task=show_command_test)
+print_result(results)
 
